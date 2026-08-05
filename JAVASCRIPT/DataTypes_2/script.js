753ae1af2cc14d11898a03b2897c0b7aed33a2ed
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-08-05 12:58:56 +0530

    Question Solved

--- JAVASCRIPT/DataTypes_2/script.js
+++ JAVASCRIPT/DataTypes_2/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+// Data Types
+// Q.1 Convert "100" into a number.
+// Q.2 Convert 500 into a string.
+// Q.3 Convert "true" into a boolean.
+// Q.4 Convert 1 into a boolean.
+// Q.5 Convert 0 into a boolean.
+// Q.6 Convert false into a number.
+// Q.7 Convert true into a number.
+// Q.8 Convert "25.6" into a number.
+// Q.9 Convert "123abc" into a number.
+// Q.10 Convert null into a number.
