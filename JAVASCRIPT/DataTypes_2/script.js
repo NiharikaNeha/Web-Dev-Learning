3cfd33f5690fae8526b78a4ec9e68533da9eda16
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-08-05 13:09:41 +0530

    Question Solved

--- JAVASCRIPT/DataTypes_2/script.js
+++ JAVASCRIPT/DataTypes_2/script.js
@@ -12,28 +12,29 @@ let num3 = "true";
 console.log(typeof(Boolean(num3)));
 
 // Q.4 Convert 1 into a boolean.
-let num = "100";
-console.log(typeof(Number(num)));
+let num4 = 1;
+console.log(typeof(Boolean(num4)));
 
 // Q.5 Convert 0 into a boolean.
-let num = "100";
-console.log(typeof(Number(num)));
+let num5 = 0;
+console.log(typeof(Boolean(num5)));
 
 // Q.6 Convert false into a number.
-let num = "100";
-console.log(typeof(Number(num)));
+let num6 = "False";
+console.log(typeof(Number(num6)));
 
 // Q.7 Convert true into a number.
-let num = "100";
-console.log(typeof(Number(num)));
+let num7 = true;
+console.log(typeof(Number(num7)));
 
 // Q.8 Convert "25.6" into a number.
-let num = "100";
-console.log(typeof(Number(num)));
+let num8 = "25.6";
+console.log(typeof(Number(num8)));
 
-// Q.9 Convert "123abc" into a number.let num = "100";
-console.log(typeof(Number(num)));
+// Q.9 Convert "123abc" into a number.
+let num9 = "123abc";
+console.log(typeof(Number(num9)));
 
 // Q.10 Convert null into a number.
-let num = "100";
-console.log(typeof(Number(num)));
+let num10 = null;
+console.log(typeof(Number(num10)));
