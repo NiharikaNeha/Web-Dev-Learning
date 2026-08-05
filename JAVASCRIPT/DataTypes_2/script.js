9a233467e3addfcb22c1772395a84607b5b90753
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-08-05 13:04:03 +0530

    Question Solved

--- JAVASCRIPT/DataTypes_2/script.js
+++ JAVASCRIPT/DataTypes_2/script.js
@@ -1,11 +1,39 @@
 // Data Types
 // Q.1 Convert "100" into a number.
+let num = "100";
+console.log(typeof(Number(num)));
+
 // Q.2 Convert 500 into a string.
+let num = "100";
+console.log(typeof(Number(num)));
+
 // Q.3 Convert "true" into a boolean.
+let num = "100";
+console.log(typeof(Number(num)));
+
 // Q.4 Convert 1 into a boolean.
+let num = "100";
+console.log(typeof(Number(num)));
+
 // Q.5 Convert 0 into a boolean.
+let num = "100";
+console.log(typeof(Number(num)));
+
 // Q.6 Convert false into a number.
+let num = "100";
+console.log(typeof(Number(num)));
+
 // Q.7 Convert true into a number.
+let num = "100";
+console.log(typeof(Number(num)));
+
 // Q.8 Convert "25.6" into a number.
-// Q.9 Convert "123abc" into a number.
+let num = "100";
+console.log(typeof(Number(num)));
+
+// Q.9 Convert "123abc" into a number.let num = "100";
+console.log(typeof(Number(num)));
+
 // Q.10 Convert null into a number.
+let num = "100";
+console.log(typeof(Number(num)));
