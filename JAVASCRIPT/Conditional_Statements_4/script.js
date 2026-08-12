75b85a483c9f9e0d3c0e2fde54e479aa9c8aca8b
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-08-12 12:23:50 +0530

    Question Solved

--- JAVASCRIPT/Conditional_Statements_4/script.js
+++ JAVASCRIPT/Conditional_Statements_4/script.js
@@ -11,37 +11,43 @@ if (num1 <= 0) {
 };
 
 // Q.3 Check whether a person is 18 or older.
-let num2 = 19;
-if (num2 >= 18) {
+let age = 19;
+if (age >= 18) {
     console.log("The Person Is Eligible For Vote.");
 };
 
 // Q.4 Check whether a student has scored more than 40 marks.
-let num3 = 75;
-if (num3 >= 40) {
+let marks = 75;
+if (marks >= 40) {
     console.log("The Student has Properly Passed.");
 };
 
 // Q.5 Check whether a number is greater than 100.
-let num4 = 75;
-if (num4 > 100) {
+let num3 = 75;
+if (num3 > 100) {
     console.log("The Number Is Greater Than 100.");
 };
 
 // Q.6 Check whether a number is divisible by 5.
-let num5 = 75;
-if (num5 % 5 === 0) {
+let num4 = 75;
+if (num4 % 5 === 0) {
     console.log("The Number Is Divisible By 5.")
 }
 
 // Q.7 Check whether a number is even.
-let num6 =  75;
-if (num6 % 2 === 0) {
+let num5 =  75;
+if (num5 % 2 === 0) {
     console.log("The Number Is Even.");
 };
 
 // Q.8 Check whether a person is eligible to vote.
-let num7 = 20;
-if (num7 >= 18) {
+let age1 = 20;
+if (age1 >= 18) {
     console.log("The Person Is Eligible TO Vote.");
+};
+
+// Q.9 Check whether the temperature is greater than 30.
+let temp = 28;
+if (temp >= 30) {
+    console.log("The Temperature Is The Greater Than 30.");
 };
