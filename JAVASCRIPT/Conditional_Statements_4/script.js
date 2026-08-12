61030cd6ca6b96dd7d515defcf07dab368571444
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-08-12 11:12:26 +0530

    Question Solved

--- JAVASCRIPT/Conditional_Statements_4/script.js
+++ JAVASCRIPT/Conditional_Statements_4/script.js
@@ -3,3 +3,9 @@ let num = 75;
 if (num >= 0) {
     console.log("The Number Is Positive");
 };
+
+// Q.2 Check whether a number is negative.
+let num1 = -70;
+if (num1 <= 0) {
+    console.log("The Number Is Negative");
+};
