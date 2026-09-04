885aeb738e3de5672aa83a742b2fe7d302540159
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-09-04 20:58:40 +0530

    completed question

--- JAVASCRIPT/Functions_5/script.js
+++ JAVASCRIPT/Functions_5/script.js
@@ -11,8 +11,15 @@
 // name();
 
 // Create a function that takes two numbers and returns their sum.
-function sum(a, b) {
-  total = a + b;
-  console.log(`The Sum Of ${a} and ${b} = `, total);
+// function sum(a, b) {
+//   total = a + b;
+//   console.log(`The Sum Of ${a} and ${b} = `, total);
+// }
+// sum(50, 10);
+
+// Create a function that takes two numbers and returns their difference.
+function differ(a, b) {
+  total = a - b;
+  console.log(`The Difference Of ${a} and ${b} = `, total);
 }
-sum(50, 10);
+differ(50, 10);
