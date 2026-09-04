c3af957f76f1a3e11d58ba2c46e2f3a6a33c63ab
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-09-04 19:48:21 +0530

    completed question 2

--- JAVASCRIPT/Functions_5/script.js
+++ JAVASCRIPT/Functions_5/script.js
@@ -1,5 +1,11 @@
 // Create a function to print "Hello World".
-function hello() {
-    console.log("Hello World!");
-};
-hello();
+// function hello() {
+//     console.log("Hello World!");
+// };
+// hello();
+
+//  Create a function that takes a name as a parameter and prints:
+function name(name = "Neha"){
+    console.log(`Hello, ${name}`)
+}
+name();
