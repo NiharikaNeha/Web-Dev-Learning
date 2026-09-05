f7936891b5df551a6334f3746cda144be0f9b2c0
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-09-05 13:07:45 +0530

    completed question

--- JAVASCRIPT/Functions_5/script.js
+++ JAVASCRIPT/Functions_5/script.js
@@ -1,120 +1,120 @@
 // Create a function to print "Hello World".
-// function hello() {
-//     console.log("Hello World!");
-// };
-// hello();
+function hello() {
+    console.log("Hello World!");
+};
+hello();
 
 // Create a function that takes a name as a parameter and prints:
-// function name(name = "Neha"){
-//     console.log(`Hello, ${name}`)
-// }
-// name();
+function name(name = "Neha"){
+    console.log(`Hello, ${name}`)
+}
+name();
 
 // Create a function that takes two numbers and returns their sum.
-// function sum(a, b) {
-//   total = a + b;
-//   console.log(`The Sum Of ${a} and ${b} = `, total);
-// }
-// sum(50, 10);
+function sum(a, b) {
+  total = a + b;
+  console.log(`The Sum Of ${a} and ${b} = `, total);
+}
+sum(50, 10);
 
 // Create a function that takes two numbers and returns their difference.
-// function differ(a, b) {
-//   total = a - b;
-//   console.log(`The Difference Of ${a} and ${b} = `, total);
-// }
-// differ(50, 10);
+function differ(a, b) {
+  total1 = a - b;
+  console.log(`The Difference Of ${a} and ${b} = `, total1);
+}
+differ(50, 10);
 
 // Create a function that takes two numbers and returns their multiplication.
-// function multiply(a, b) {
-//   total = a * b;
-//   console.log(`The Multiplication Of ${a} and ${b} = `, total);
-// }
-// multiply(50, 10);
+function multiply(a, b) {
+  total2 = a * b;
+  console.log(`The Multiplication Of ${a} and ${b} = `, total2);
+}
+multiply(50, 10);
 
 // Create a function that takes a number and returns its square.
-// function double(a) {
-//   total = a ** 2;
-//   console.log(`The Square Of ${a} = `, total);
-// }
-// double(500);
+function double(a) {
+  total3 = a ** 2;
+  console.log(`The Square Of ${a} = `, total3);
+}
+double(500);
 
 // Create a function that takes a number and checks whether it is positive, negative, or zero.
-// const checks = (num) => {
-//     if (num > 0){
-//         return(`${num} is a Positive Number.`);
-//     }
-//     else if (num < 0) {
-//         return(`${num} is a Negative Number.`);
-//     } else {
-//         return(`${num} is Equal To Zero.`);
-//     }
-// }
-// console.log(checks(0));
+const checks = (num) => {
+    if (num > 0){
+        return(`${num} is a Positive Number.`);
+    }
+    else if (num < 0) {
+        return(`${num} is a Negative Number.`);
+    } else {
+        return(`${num} is Equal To Zero.`);
+    }
+}
+console.log(checks(0));
 
 // Create a function that takes a number and checks whether it is even or odd.
-// let func = (num1) => {
-//   if (num1 % 2 == 0) {
-//     return `${num1} is a even number.`;
-//   } else {
-//     return `${num1} is a odd number.`;
-//   }
-// };
+let func = (num1) => {
+  if (num1 % 2 == 0) {
+    return `${num1} is a even number.`;
+  } else {
+    return `${num1} is a odd number.`;
+  }
+};
 
-// console.log(func(23));
+console.log(func(23));
 
 // Create a function that takes a number and checks whether it is divisible by 5.
-// let check = (num2) => {
-//     if (num2 % 5 == 0) {
-//         return(`${num2} is divisible by 5`)
-//     } else {
-//         return(`${num2} is not divisible by 5`)
-//     }
-// }
-// console.log(check(30))
+let check = (num2) => {
+    if (num2 % 5 == 0) {
+        return(`${num2} is divisible by 5`)
+    } else {
+        return(`${num2} is not divisible by 5`)
+    }
+}
+console.log(check(30))
 
 // Create a function that takes a person's age and returns:
 // "Child" if age < 13
 // "Teenager" if age is 13–19
 // "Adult" if age >= 20
-// let ageNum = (age) => {
-//   if (age < 13) {
-//     return "He/She Is A Child.";
-//   } else if (age >= 13 && age <= 19) {
-//     return "He/She Is A Teenager";
-//   } else if (age >= 20) {
-//     return "He/She Is An Adult";
-//   } else {
-//     return "Invalid Age. Try Again Later!";
-//   }
-// };
+let ageNum = (age) => {
+  if (age < 13) {
+    return "He/She Is A Child.";
+  } else if (age >= 13 && age <= 19) {
+    return "He/She Is A Teenager";
+  } else if (age >= 20) {
+    return "He/She Is An Adult";
+  } else {
+    return "Invalid Age. Try Again Later!";
+  }
+};
 
-// console.log(ageNum(30));
+console.log(ageNum(30));
 
 // Create a function that takes marks and returns:
 // A → 90 or above
 // B → 75–89
 // C → 50–74
 // Fail → below 50
-// let marks = (num3) => {
-//   if (num3 >= 90) {
-//     return "Grade A";
-//   } else if (num3 >= 75 && num3 <= 89) {
-//     return "Grade B";
-//   } else if (num3 >= 50 && num3 <= 74) {
-//     return "Grade C";
-//   } else {
-//     return "Fail";
-//   }
-// };
+let marks = (num3) => {
+  if (num3 >= 90) {
+    return "Grade A";
+  } else if (num3 >= 75 && num3 <= 89) {
+    return "Grade B";
+  } else if (num3 >= 50 && num3 <= 74) {
+    return "Grade C";
+  } else {
+    return "Fail";
+  }
+};
 
-// console.log(marks(100));
+console.log(marks(100));
 
 // Create a function that takes two numbers and returns the greater number.
-// function number(a,b) {
-//     if (a > b) {
-//         return `${a} is greater than ${b}`
-//     } else {
-//         return `${b} is greater than ${a}`
-//     }
-// }
-// console.log(number(75,57))
+function number(a,b) {
+    if (a > b) {
+        return `${a} is greater than ${b}`
+    } else {
+        return `${b} is greater than ${a}`
+    }
+}
+console.log(number(75,57))
